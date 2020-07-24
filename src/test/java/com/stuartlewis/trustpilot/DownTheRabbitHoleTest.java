b4d64b7889cfa5e4aa23999a40aa5a0016561ef3
Stuart Lewis <stuart@stuartlewis.com>
2020-07-24 20:10:59 +0100

    Adding more tests

--- src/test/java/com/stuartlewis/trustpilot/DownTheRabbitHoleTest.java
+++ src/test/java/com/stuartlewis/trustpilot/DownTheRabbitHoleTest.java
@@ -1,8 +1,10 @@
 package com.stuartlewis.trustpilot;
 
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,10 @@ import org.junit.jupiter.api.Test;
  * @author Stuart Lewis
  */
 public class DownTheRabbitHoleTest {
+    
+    /**
+     * Test the method which looks to see if two words are true anagrams
+     */
     @Test
     public void isAnagram() {
         assertTrue(DownTheRabbitHole.isAnagram("trustpilot", "toliptsurt"), "The same letters should be an anagram");
@@ -19,8 +25,47 @@ public class DownTheRabbitHoleTest {
         assertTrue(DownTheRabbitHole.isAnagram("trustpilot", "to lip tsurt"), "The same letters should be an anagram including spaces");
     }
     
+    /**
+     * Test the MD5 hash generation
+     */
     @Test
     public void md5() {
         assertEquals(DownTheRabbitHole.md5("trustpilot"), "f5ad76ee64505e3ae887b634e05f30be", "trustpilot as an MD5 hash should be f5ad76ee64505e3ae887b634e05f30be");
+        assertNotEquals(DownTheRabbitHole.md5("trustpilot"), "X5ad76ee64505e3ae887b634e05f30bX", "trustpilot as an MD5 hash should be f5ad76ee64505e3ae887b634e05f30be"); 
+    }
+    
+    /**
+     * Test the method that removes words from phrases once a candidate word has been chosen
+     */
+    @Test
+    public void removeWordFromPhrase() {
+        assertEquals(DownTheRabbitHole.removeWordFromPhrase("myphrase", "phrase"), "my", "Removing phrase from myphrase should leave my");
+        assertNotEquals(DownTheRabbitHole.removeWordFromPhrase("myphrase", "phrase"), "phrase", "Removing phrase from myphrase should leave my");
+    }
+    
+    /**
+     * Test the candidate reduction code
+     */
+    @Test
+    public void reduce() {
+        // The candidate words
+        ArrayList<String> testWords = new ArrayList<>();
+        testWords.add("this");
+        testWords.add("this"); // Added twice to check duplciates are removed
+        testWords.add("is");
+        testWords.add("a");
+        testWords.add("unit");
+        testWords.add("test");
+        
+        // Test phrase: unit test backwards
+        String phrase = "tsettniu";
+        
+        // The words that could be in the anagram
+        ArrayList<String> possibles = new ArrayList<>();
+        possibles.add("is");
+        possibles.add("unit");
+        possibles.add("test");
+        
+        assertEquals(DownTheRabbitHole.reduce(testWords, phrase, true), possibles, "We expect to see is / unit / test");
     }
 }
